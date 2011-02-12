88705a11ef78a3b8ee43d392a9517e740542b70e
pierrchen <pierr.chen@gmail.com>
2011-02-12 13:34:35 +0800

    a skelton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jdcc::Application.config.secret_token = 'a3e8c2ded8d60d768e95f6824981f57bef98e54c5258cd0687f976835dfba59b6f415ab83f073733f22d741a730cf1e6f373405ea0257183b197870ae9bec71f'
